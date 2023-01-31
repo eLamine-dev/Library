481cd0c3085a5564bab7c5d53867466d400cbf29
e-lamine <lamine@e-lamine.dev>
2023-01-31 18:58:43 +0100

    added remove book and toggle read logic

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-console */
+const booksContainer = document.getElementById('books-container');
+const newBookBtn = document.getElementById('new-book-btn');
+const closeNewBookForm = document.getElementById('cancel-book-btn');
+const formModal = document.getElementById('form-modal');
+const newBookForm = document.getElementById('new-book-form');
+
 const library = [];
 
 class Book {
@@ -13,44 +20,56 @@ class Book {
    }
 }
 
-function addBook() {
-   const author = document.getElementById('author').value;
-   const title = document.getElementById('title').value;
-   const pages = document.getElementById('pages').value;
-   const read = document.getElementById('read').checked;
-   library.push(new Book(author, title, pages, read));
+function addBook(author, title, pages, read) {
+   const newBook = new Book(author, title, pages, read);
+   library.push(newBook);
+   updateLibrary(newBook);
 }
 
-const newBookBtn = document.getElementById('new-book-btn');
-const closeNewBookForm = document.getElementById('cancel-book-btn');
-const formModal = document.getElementById('form-modal');
-const newBookForm = document.getElementById('new-book-form');
-const booksContainer = document.getElementById('books-container');
-
-function updateLibrary() {
-   for (let i = library.length - 1; i < library.length; i++) {
-      const bookCard = document.createElement('div');
-      bookCard.classList.add('book-card');
-      bookCard.innerHTML = `
-      <h2>${library[i].title}</h2>
-      <p>Author: ${library[i].author}</p>
-      <p>Pages: ${library[i].pages}</p>
-      <p>Read: ${library[i].read ? 'Yes' : 'No'}</p>
-      <button class="remove-book" data-index="${i}">Remove Book</button>
-      <button class="toggle-read" data-index="${i}">Toggle Read</button>
+function updateLibrary(newBook) {
+   const bookCard = document.createElement('div');
+   const index = library.indexOf(newBook);
+   bookCard.classList.add('book-card');
+   bookCard.innerHTML = `
+      <h2>${newBook.title}</h2>
+      <p>Author: ${newBook.author}</p>
+      <p>Pages: ${newBook.pages}</p>
+      <p id="read">Read: ${newBook.read ? 'Yes' : 'No'}</p>
+      <button class="remove-book">Remove Book</button>
+      <button class="toggle-read">Toggle Read</button>
     `;
+   booksContainer.appendChild(bookCard);
 
-      booksContainer.appendChild(bookCard);
-   }
+   bookCard.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (event.target.classList.contains('remove-book')) {
+         library.splice(index, 1);
+         bookCard.remove();
+
+         console.log(library);
+         console.log(booksContainer);
+      } else if (event.target.classList.contains('toggle-read')) {
+         newBook.toggleRead();
+         bookCard.querySelector('#read').textContent = `Read: ${
+            newBook.read ? 'Yes' : 'No'
+         }`;
+      }
+   });
 }
 
 newBookForm.addEventListener('submit', (event) => {
    event.preventDefault();
-   addBook();
+   console.log(event);
+   const author = newBookForm.elements.author.value;
+   const title = newBookForm.elements.title.value;
+   const pages = newBookForm.elements.pages.value;
+   const read = newBookForm.elements.read.checked;
+
+   addBook(author, title, pages, read);
    formModal.close();
    newBookForm.reset();
    console.log(library);
-   updateLibrary();
+   console.log(booksContainer);
 });
 
 newBookBtn.addEventListener('click', () => {
@@ -60,3 +79,22 @@ newBookBtn.addEventListener('click', () => {
 closeNewBookForm.addEventListener('click', () => {
    formModal.close();
 });
+
+// booksContainer.addEventListener('click', (event) => {
+//    const bookCardIndex = event.target.dataset.index;
+//    const targetBook = document.querySelector(
+//       `[data-index="${bookCardIndex}"]`
+//    );
+//    if (event.target.classList.contains('remove-book')) {
+//       delete library[bookCardIndex];
+//       booksContainer.removeChild(targetBook);
+
+//       console.log(library);
+//       console.log(booksContainer);
+//    } else if (event.target.classList.contains('toggle-read')) {
+//       library[bookCardIndex].toggleRead();
+//       targetBook.querySelector('#read').textContent = `Read: ${
+//          library[bookCardIndex].read ? 'Yes' : 'No'
+//       }`;
+//    }
+// });
